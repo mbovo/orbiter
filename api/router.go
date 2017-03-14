517ebdadd9469ab2f6b8ebbb2bdeb019e9a53e11
Gianluca Arbezzano <gianarb92@gmail.com>
2017-03-14 21:44:01 +0000

    WIP

--- api/router.go
+++ api/router.go
@@ -4,6 +4,7 @@ import (
 	"github.com/Sirupsen/logrus"
 	"github.com/gianarb/orbiter/core"
 	"github.com/gorilla/mux"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func GetRouter(core core.Core, eventChannel chan *logrus.Entry) *mux.Router {
@@ -11,5 +12,6 @@ func GetRouter(core core.Core, eventChannel chan *logrus.Entry) *mux.Router {
 	r.HandleFunc("/handle/{autoscaler_name}/{service_name}", Handle(core.Autoscalers)).Methods("POST")
 	r.HandleFunc("/health", Health()).Methods("GET")
 	r.HandleFunc("/events", Events(eventChannel)).Methods("GET")
+	r.Handle("/metrics", promhttp.Handler())
 	return r
 }
